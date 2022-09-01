42dbbdcc600ffd863d1f79f1fb4006927ee98958
Son Hong Su <son.ho463@gmail.com>
2022-09-01 19:03:18 +0900

    fix when nodata

--- src/components/VoiceList.js
+++ src/components/VoiceList.js
@@ -8,15 +8,8 @@ import { ReactUseAudio } from "components/ReactAudioPlayer"
 // サウンドファイルの名前をgraphqlで取得
 
 export const VoiceList = props => {
-  const {
-    countryId,
-    //  countryName, voice
-  } = props
+  const { countryId } = props
   const itemList = []
-
-  // const Sound = KumeBye
-  // const [play, { stop, pause }] = useSound(Sound)
-  // console.log(props.greetingsEdges[0])
   props.greetingsEdges.forEach(ele => {
     // console.log(ele.node.japanese.greeting_word)
     const vPath = `/assets/voices/${ele.node.voice}.mp3`
@@ -30,48 +23,21 @@ export const VoiceList = props => {
           <div>
             <ReactUseAudio src={vPath} />
           </div>
-
-          {/* <nav className="button-wrapper">
-            <button onClick={() => play()}>再生</button>
-            <button onClick={() => stop()}>停止</button>
-            <button onClick={() => pause()}>一時停止</button>
-          </nav> */}
         </li>
       )
     }
   })
-  // console.log(countryId)
-  // console.log(countryName)
-  // props.greetingEdges.map((node)=>{
-  // 	console.log(node.japanese.greeting_word)
-  // })
+if(itemList.length===0){
+	itemList.push(
+		<li>表示するデータがありません。</li>
+	)
+}
 
   return (
     <ul className="voice_list">
+
       {itemList}
-      {/* {greetingsEdges.map(({ node }) => {
-        // console.log(node.country.id === countryId)
-        // console.log(node.country.id)
-        // console.log(countryId)
-        if (node.country.id === countryId) {
-          return (
-            <li key="{node.id}" className="voice">
-              <figure>
-                <figcaption>
-                  {node.japanese.greeting_word}：{node.foreign}
-                </figcaption>
-                <AudioButtons />
-                <audio controls="" src="./voices/kor_morning.mp3">
-                  Your browser does not support the
-                  <code>audio</code> element.
-                </audio>
-              </figure>
-            </li>
-          )
-        } else {
-          return null
-        }
-      })} */}
+
     </ul>
   )
 }
